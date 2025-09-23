f5c12865352f3535fc9e6a01e45bf260f396cf4e
devtofunmi <olayiwolajesutofunmi@gmail.com>
2025-09-23 20:55:34 +0100

    add a backicon

--- src/pages/popup/icons.tsx
+++ src/pages/popup/icons.tsx
@@ -50,3 +50,9 @@ export const DetailedSummaryIcon = () => (
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12h18M3 6h18M3 18h18" />
     </svg>
 );
+
+export const BackIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+    </svg>
+  );
